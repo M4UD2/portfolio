25f7f6cf3814562c10ce5c869fcfa258f2d01165
liviaaoli <71132950+liviaaoli@users.noreply.github.com>
2025-12-23 13:53:44 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-// CORREÇÃO: Importando o globals.css onde colocamos o @import "tailwindcss"
-import "./styles/globals.css"; 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './styles/globals.css' // <--- O IMPORTANTE: Apontando para o arquivo certo
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
